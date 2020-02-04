f53f820c6bc7110dd8dbc1a4321abf79d1c51fa3
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-02-04 13:50:47 +0100

    define implementation for interface, reference interface in spring file, test application

---
+++ Activity#1/src/activity1/inversionofcontrol/xmlconfiguration/OtherCoach.java
@@ -0,0 +1,15 @@
+package activity1.inversionofcontrol.xmlconfiguration;
+
+public class OtherCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return "ooo";
+	}
+
+}
