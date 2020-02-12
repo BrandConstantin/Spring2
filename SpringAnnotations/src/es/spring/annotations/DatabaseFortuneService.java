23f7180bc8539bffa4bd526b54ce62ba55e518ef
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-02-12 23:56:54 +0100

    qualifiers for dependency injection

---
+++ SpringAnnotations/src/es/spring/annotations/DatabaseFortuneService.java
@@ -0,0 +1,13 @@
+package es.spring.annotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
---
+++ SpringAnnotations/src/es/spring/annotations/RESTFortuneService.java
@@ -0,0 +1,13 @@
+package es.spring.annotations;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RESTFortuneService implements FortuneService {
+
+	@Override
+	public String getFortune() {
+		return null;
+	}
+
+}
