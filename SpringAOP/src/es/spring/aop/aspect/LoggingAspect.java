6bf91a9b2f353043587dd58a3929adf666ea3051
Constantin Brindusoiu <BrandConstantin@users.noreply.github.com>
2020-03-04 16:49:39 +0100

    AOP pointcut expressions - match method parameter type overview

--- SpringAOP/src/es/spring/aop/aspect/LoggingAspect.java
+++ SpringAOP/src/es/spring/aop/aspect/LoggingAspect.java
@@ -13,7 +13,8 @@ public class LoggingAspect {
 	//@Before("execution(public void es.spring.aop.dao.AccountDAO.addAccount())") // match only addAccount method for this specific class
 	//@Before("execution(public void add*())") // match any add* method
 	//@Before("execution(void add*())") // match any return type
-	@Before("execution(* add*())") // match any return type
+	//@Before("execution(* add*())") // match any return type
+	@Before("execution(* add*(es.spring.aop.Account))") // match method with account and more params
 	public void beforeAddAccountAdvice() {
 		System.out.println(">>> executing @Before");
 	}
--- SpringAOP/src/es/spring/aop/dao/AccountDAO.java
+++ SpringAOP/src/es/spring/aop/dao/AccountDAO.java
@@ -2,9 +2,11 @@ package es.spring.aop.dao;
 
 import org.springframework.stereotype.Component;
 
+import es.spring.aop.Account;
+
 @Component
 public class AccountDAO {
-	public void addAccount() {
+	public void addAccount(Account theAccount) {
 		System.out.println(getClass() + " doing the add account");
 	}
 }
